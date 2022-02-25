2dd73875d3b41359971aae3c300498529f3b4933
Phearom Duong <duongphearom@gmail.com>
2022-02-25 18:00:41 -0500

    CSS and PHP

--- index.php
+++ index.php
@@ -8,43 +8,45 @@
 </head>
 
 <body>
-    <form method="GET" action="">
-        Username: <input id="username" type="text" name="username" placeholder="Enter your username" /><br>
-        <input type="submit" name="submit" value="Submit"/>
-    <p><?php
-    ?>
-    </p>
-    <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "music-db";
+    <div class="submit_box">
+        <form method="GET" action="">
+            Username: <input id="username" type="text" name="username" placeholder="Enter your username" /><br>
+            <input id="submission" type="submit" name="submit" value="Submit" />
+            <p><?php
+                ?>
+            </p>
+    </div>
+    <div class="phpStyle">
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "music-db";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    if (isset($_REQUEST["submit"])) {
-        $out_value =  "";
-        $s_username = $_REQUEST['username'];
-
-        if (!empty($s_username)) {
-            $sql_query = "SELECT * FROM ratings WHERE username = ('$s_username')" ;
-            $result = mysqli_query($conn, $sql_query);
-            while($row = mysqli_fetch_array($result)){
-                $out_value =  $row['song'] . " --> " . $row['ratings'] ."<br>";
-                echo $out_value;
-            }
-            
-        }else{
-            echo $out_value = "No songs available"; 
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
         }
-    }   
-    $conn->close();
-    ?>
 
+        if (isset($_REQUEST["submit"])) {
+            $out_value =  "";
+            $s_username = $_REQUEST['username'];
+
+            if (!empty($s_username)) {
+                $sql_query = "SELECT * FROM ratings WHERE username = ('$s_username')";
+                $result = mysqli_query($conn, $sql_query);
+                while ($row = mysqli_fetch_array($result)) {
+                    $out_value =  $row['song'] . " --> " . $row['ratings'] . "<br>";
+                    echo $out_value;
+                }
+            } else {
+                echo $out_value = "No songs available";
+            }
+        }
+        $conn->close();
+        ?>
+    </div>
 </body>
 
 </html>
