9ea5638c992a2ff2a2d3463f14fb9555f471ac69
Michael Renda <41514118+micoo227@users.noreply.github.com>
2022-03-02 13:25:52 -0500

    Created registration functionality

--- index.php
+++ index.php
@@ -8,13 +8,29 @@
 </head>
 
 <body>
-    <div class="submit_box">
+    <div>
+        <h1>
+            music-db
+        </h1>
+    </div>
+
+    <div>
+        <form class="form" action="" method="POST">
+            <h2>Registration</h2>
+            <span>Username: <input type="text" name="username" required /></span>
+            <span>Password: <input type="password" name="password" /></span>
+            <span><input type="submit" name="register" value="Register"></span>
+        </form>
+    </div>
+
+    <div>
         <form method="GET" action="">
             Username: <input id="username" type="text" name="username" placeholder="Enter your username" /><br>
             <input id="submission" type="submit" name="submit" value="Submit" />
             <p><?php
                 ?>
             </p>
+        </form>
     </div>
     <div class="phpStyle">
         <?php
@@ -27,6 +43,42 @@
 
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
+        }
+
+        // if user registers new account (presses register button)
+        if (isset($_POST['register'])) {
+            $out_value = "";
+            // store recorded information in variables
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            // verify that the user entered the info properly
+            if (!empty($username) and !empty($password)){
+                // query the database to see if the username is taken
+                $sql_query = "SELECT * FROM users WHERE username = '$username'";
+                $result = mysqli_query($conn, $sql_query);
+                if (mysqli_num_rows($result) == 0){
+                    // insert the user into the database
+                    $sql_query = "INSERT INTO users VALUES ('$username', '$password')";
+                    mysqli_query($conn, $sql_query);
+
+                    // verify that the account was created
+                    $sql_query = "SELECT * FROM users WHERE username = '$username'";
+                    $result = mysqli_query($conn, $sql_query);
+                    if (mysqli_num_rows($result) == 1){
+                        echo $out_value = "Account created successfully.";
+                    }
+                    else{
+                        echo $out_value = "An error occurred and your account was not created. Please try again.";
+                    }
+                }
+                else {
+                    echo $out_value = "This username is already taken. Please try again.";
+                }
+            }
+            else{
+                echo $out_value = "Please complete the required fields.";
+            }
         }
 
         if (isset($_REQUEST["submit"])) {
