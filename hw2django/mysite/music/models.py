68d4f190dded9615d54bf92f16623d9794b63842
Phearom Duong <duongphearom@gmail.com>
2022-03-05 18:55:53 -0500

    Django View

--- hw2django/mysite/music/models.py
+++ hw2django/mysite/music/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+class ratings(models.Model):
+    username = models.ForeignKey(users, on_delete=models.CASCADE)
+    song = models.CharField(max_length=255)
+    rating = models.IntegerField()
