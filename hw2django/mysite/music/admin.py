e99acdf9aa205a93ef3b1d4e401d6b79315e6498
Phearom Duong <duongphearom@gmail.com>
2022-03-05 20:45:10 -0500

    Django database

--- hw2django/mysite/music/admin.py
+++ hw2django/mysite/music/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import user, artist, rating
 # Register your models here.
+
+admin.site.register(user)
+admin.site.register(artist)
+admin.site.register(rating)
--- hw2django/mysite/music/models.py
+++ hw2django/mysite/music/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
 # Create your models here.
-class users(models.Model):
-    username = models.CharField(max_length=255)
+class user(models.Model):
+    username = models.CharField(max_length=255, primary_key=True, unique=True,null=False)
     password = models.CharField(max_length=255)
 
-class ratings(models.Model):
-    username = models.ForeignKey(users, on_delete=models.CASCADE)
-    song = models.CharField(max_length=255)
-    rating = models.IntegerField()
+    def __str__(self):
+        return self.username
+
+class artist(models.Model):
+    song = models.CharField(max_length=255, primary_key=True)
+    artist = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.song)
+
+class rating(models.Model):
+    username = models.ForeignKey(user, on_delete=models.CASCADE)
+    song = models.ForeignKey(artist, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.id)
+
+
